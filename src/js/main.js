ff806aad2c1f0f983934ca0c777cb1f28fda90c9
Muurein <amanda.l.thim@gmail.com>
2025-02-13 14:31:59 +0100

    added hamburger-menu to mobile

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/* get elements */
+const dropdownMenuEl = document.getElementById("hamburger");
+const menuListEl = document.getElementById("nav-ul");
+
+/* toggles active */
+dropdownMenuEl.addEventListener("click", () => {
+    dropdownMenuEl.classList.toggle("active");
+    menuListEl.classList.toggle("active");
+})
+
+.document.querySelectorAll(".menu-link").forEach(n => n.addEventListener("click", () => {
+    dropdownMenuEl.classList.remove("active");
+    menuListEl.classList.remove("active");
+}))
